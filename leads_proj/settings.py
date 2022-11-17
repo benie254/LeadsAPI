2438febfb128a05f228123c9699942019595fc10
benie254 <davinci.monalissa@gmail.com>
2022-11-17 08:11:22 +0300

    prep app for vercel deployment

--- leads_proj/settings.py
+++ leads_proj/settings.py
@@ -188,7 +188,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
 STATIC_URL = '/static/'
 
 # extra places for collectstatic to find static files 
